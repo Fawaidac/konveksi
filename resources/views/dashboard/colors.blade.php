723bae1104492542e3e1b798cb8021ec46a0eb1d
Fawaidac <112240727+Fawaidac@users.noreply.github.com>
2024-05-02 20:42:48 +0700

    [done] crud color picker

--- resources/views/dashboard/colors.blade.php
+++ resources/views/dashboard/colors.blade.php
@@ -35,8 +35,10 @@
                                         style="background: {{ $item->code_color }}">{{ $item->code_color }}</a>
                                 </td>
                                 <td>
-                                    <button class="btn icon btn-primary"><i class="bi bi-pencil"></i></button>
-                                    <button class="btn icon btn-danger"><i class="bi bi-trash"></i></button>
+                                    <button class="btn icon btn-primary" data-bs-toggle="modal"
+                                        data-bs-target="#update{{ $item->id }}"><i class="bi bi-pencil"></i></button>
+                                    <button class="btn icon btn-danger" data-bs-toggle="modal"
+                                        data-bs-target="#delete{{ $item->id }}"><i class="bi bi-trash"></i></button>
                                 </td>
                             </tr>
                         @endforeach
@@ -90,6 +92,89 @@
             </div>
         </div>
     </div>
+    {{-- modal edit --}}
+    @foreach ($colors as $item)
+        <div class="modal fade text-left" id="update{{ $item->id }}" tabindex="-1" role="dialog"
+            aria-labelledby="myModalLabel1" data-bs-backdrop="false" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-scrollable modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="myModalLabel1">Edit Data Warna</h5>
+                        <button type="button" class="close rounded-pill" data-bs-dismiss="modal" aria-label="Close">
+                            <i data-feather="x"></i>
+                        </button>
+                    </div>
+                    <form action="{{ route('colors.update', $item->id) }}" method="post">
+                        @csrf
+                        @method('PUT')
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="first-name-vertical">Nama Warna</label>
+                                <input type="text" id="first-name-vertical" required value="{{ $item->name_color }}"
+                                    class="form-control" name="name_color" placeholder="Name" />
+                            </div>
+                            <div class="form-group">
+                                <label for="color">Pilih Warna</label>
+                                <div class="input-group">
+                                    <div class="me-2">
+                                        <div class="color-picker-edit" id="color_edit_{{ $item->id }}"></div>
+                                    </div>
+                                    <input type="text" id="color_hex_edit_{{ $item->id }}" required
+                                        name="code_color" value="{{ $item->code_color }}" class="form-control" />
+                                </div>
+                            </div>
+                            <div class="card-body px-4 py-5" style="background: {{ $item->code_color }};"
+                                id="panel{{ $item->id }}"></div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn" data-bs-dismiss="modal">
+                                <i class="bx bx-x d-block d-sm-none"></i>
+                                <span class="d-none d-sm-block">Tutup</span>
+                            </button>
+                            <button type="submit" class="btn btn-primary ms-1">
+                                <i class="bx bx-check d-block d-sm-none"></i>
+                                <span class="d-none d-sm-block">Simpan</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+    {{-- modal delete --}}
+    @foreach ($colors as $item)
+        <div class="modal fade text-left" id="delete{{ $item->id }}" tabindex="-1" role="dialog"
+            aria-labelledby="myModalLabel1" data-bs-backdrop="false" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-scrollable" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="myModalLabel1">Hapus Data Warna</h5>
+                        <button type="button" class="close rounded-pill" data-bs-dismiss="modal" aria-label="Close">
+                            <i data-feather="x"></i>
+                        </button>
+                    </div>
+                    <form action="{{ route('colors.delete', $item->id) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <div class="modal-body">
+                            Apakah anda yakin untuk menghapus data ?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn" data-bs-dismiss="modal">
+                                <i class="bx bx-x d-block d-sm-none"></i>
+                                <span class="d-none d-sm-block">Tutup</span>
+                            </button>
+                            <button type="submit" class="btn btn-primary ms-1">
+                                <i class="bx bx-check d-block d-sm-none"></i>
+                                <span class="d-none d-sm-block">Simpan</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    @endforeach
 @endsection
 @push('scripts')
     <script src="{{ asset('assets/extensions/simple-datatables/umd/simple-datatables.js') }}"></script>
@@ -147,13 +232,67 @@
         var colorInput = document.getElementById("color_hex");
         var panel = document.getElementById("panel");
 
-        // Mendengarkan perubahan pada input
         colorInput.addEventListener("input", function() {
-            // Mendapatkan nilai kode warna dari input
             var colorCode = colorInput.value;
 
-            // Menetapkan nilai kode warna sebagai background style dari panel
             panel.style.background = colorCode;
         });
+    </script>
+    <script>
+        @foreach ($colors as $item)
+            const pickrEdit_{{ $item->id }} = Pickr.create({
+                el: '.color-picker-edit', // Menggunakan class baru
+                theme: 'classic',
+
+                swatches: [
+                    'rgba(244, 67, 54, 1)',
+                    'rgba(233, 30, 99, 0.95)',
+                    'rgba(156, 39, 176, 0.9)',
+                    'rgba(103, 58, 183, 0.85)',
+                    'rgba(63, 81, 181, 0.8)',
+                    'rgba(33, 150, 243, 0.75)',
+                    'rgba(3, 169, 244, 0.7)',
+                    'rgba(0, 188, 212, 0.7)',
+                    'rgba(0, 150, 136, 0.75)',
+                    'rgba(76, 175, 80, 0.8)',
+                    'rgba(139, 195, 74, 0.85)',
+                    'rgba(205, 220, 57, 0.9)',
+                    'rgba(255, 235, 59, 0.95)',
+                    'rgba(255, 193, 7, 1)'
+                ],
+
+                components: {
+                    preview: true,
+                    opacity: true,
+                    hue: true,
+                    palette: false,
+                    interaction: {
+                        hex: true,
+                        rgba: true,
+                        hsla: true,
+                        hsva: true,
+                        cmyk: true,
+                        input: true,
+                        clear: false,
+                        save: false
+                    }
+                }
+            });
+
+            pickrEdit_{{ $item->id }}.on('change', (color, _source, _instance) => {
+                console.log('Event: "change"', color);
+                const hexColor = color.toHEXA().toString();
+                document.getElementById('panel{{ $item->id }}').style.backgroundColor = hexColor;
+                document.getElementById('color_hex_edit_{{ $item->id }}').value = hexColor;
+            });
+
+            var colorInput = document.getElementById("color_hex_edit_{{ $item->id }}");
+            var panel = document.getElementById("panel{{ $item->id }}");
+
+            colorInput.addEventListener("input", function() {
+                var colorCode = colorInput.value;
+                panel.style.background = colorCode;
+            });
+        @endforeach
     </script>
 @endpush
